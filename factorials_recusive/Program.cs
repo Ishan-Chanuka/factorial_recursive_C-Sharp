3c84bafa0741bf9544cad94bda9f3d4704eb5c10
Ishan-Chanuka <ickodippiliarachchi@students.nsbm.lk>
2021-06-22 19:19:17 +0530

    factorial recursive c sharp

---
+++ factorials_recusive/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace factorials_recusive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no;
+
+
+            Console.WriteLine("Enter a number");
+            no = Int16.Parse(Console.ReadLine());
+
+            //int fact = factorials(no);
+
+            Console.WriteLine("Factorials of " + no + " is " + factorials(no));
+
+        }
+
+        private static int factorials(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * factorials(number - 1);
+            }
+        }
+    }
+}
